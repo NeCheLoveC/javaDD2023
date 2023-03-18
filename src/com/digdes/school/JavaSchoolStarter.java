e490736e9c3a30cd919b99429b19d2437c2137fa
Anton Protsenko <protseng@mail.ru>
2023-03-18 18:25:48 +0400

    initial project

---
+++ src/com/digdes/school/JavaSchoolStarter.java
@@ -0,0 +1,18 @@
+package com.digdes.school;
+
+import java.util.List;
+import java.util.Map;
+
+public class JavaSchoolStarter
+{
+    List<Map<String, Object>> items;
+    public JavaSchoolStarter()
+    {
+
+    }
+
+    List<Map<String, Object>> execute(String query)
+    {
+        return null;
+    }
+}
